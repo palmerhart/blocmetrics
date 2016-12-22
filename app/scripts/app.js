ea011ee73a5789255dc0536f06842697c8e435ef
palmer <palmerhart@gmail.com>
2016-12-21 20:33:51 -0500

    angular-3-assignment: completed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,13 +8,17 @@
         
         $stateProvider
             .state('landing', {
-            url: '/',
-            templateUrl: '/templates/landing.html'
-        })
-        .state('album', {
-            url: '/album',
-            templateUrl: '/templates/album.html'
-        });
+                url: '/',
+                templateUrl: '/templates/landing.html'
+            })
+            .state('album', {
+                url: '/album',
+                templateUrl: '/templates/album.html'
+            })
+            .state('collection', {
+                url:'/collection',
+                templateUrl:'templates/collection.html',   
+            });
     }
     angular
         .module('blocJams', ['ui.router'])
