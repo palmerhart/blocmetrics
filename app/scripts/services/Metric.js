63b6abdf683fc16394da8099fb412f979d2ccc74
Palmer Hart <palmerhart@gmail.com>
2017-06-17 22:15:24 +0000

    problems with Metric.js service linking

--- app/scripts/services/Metric.js
+++ app/scripts/services/Metric.js
@@ -1,15 +1,29 @@
 (function() {
     function Metric($rootScope, SongPlayer) {
-        $rootScope.songPlays = ["test"];
-        $rootScope.test = "test txt";
+        $rootScope.songPlays = [];
+        
+        var currentSong = SongPlayer.currentSong;
+        var songTest = "eureka!";
         
         return {
+            //establish current song as SongObj
+            //setSongObj: function() {
+            //    if (SongPlayer.currentSong === "") {
+            //       songObj = {title: "no title", artist: "no artist"};
+            //    } else {
+            //        songObj = {title: currentSong.title, artist: currentSong.artist};
+            //}
+            
+            //var currentSong = SongPlayer.currentSong;
+            //var songTest = "eureka!";
+    
             //function that records a metric object by pushing it to the $rootScope array
             registerSongPlay: function(songObj) {
                 //add time to event register
                 songObj['playedAt'] = moment(new Date());
                 $rootScope.songPlays.push(songObj);
             },
+            
             listSongsPlayed: function() {
                 var songs = ["test5"];
                 angular.forEach($rootScope.songPlays, function(song) {
@@ -17,7 +31,7 @@
                 });
                 return songs;
             }
-        };
+        };    
     }
     
     angular
--- dist/scripts/controllers/MetricCtrl.js
+++ dist/scripts/controllers/MetricCtrl.js
@@ -1,10 +1,11 @@
 (function() {
-    function MetricCtrl($rootScope, SongPlayer, Fixtures) {
+    function MetricCtrl($rootScope, SongPlayer, Fixtures, Metric) {
         this.albumData = Fixtures.getAlbum();
         this.songPlayer = SongPlayer;
+        this.metric = Metric;
     }
     
     angular
         .module('blocJams')
-        .controller('MetricCtrl', ['$rootScope','SongPlayer', 'Fixtures', MetricCtrl]);
+        .controller('MetricCtrl', ['$rootScope','SongPlayer', 'Fixtures', 'Metric', MetricCtrl]);
 })();
