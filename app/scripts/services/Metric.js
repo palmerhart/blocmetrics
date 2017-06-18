61ab226d2062958d1023593f02a4f7a91ffc9c95
Palmer Hart <palmerhart@gmail.com>
2017-06-18 17:12:38 +0000

    06.18 work on Metric.js

--- app/scripts/services/Metric.js
+++ app/scripts/services/Metric.js
@@ -1,37 +1,38 @@
 (function() {
     function Metric($rootScope, SongPlayer) {
+        var Metric = {};
+        
         $rootScope.songPlays = [];
         
         var currentSong = SongPlayer.currentSong;
         var songTest = "eureka!";
         
-        return {
-            //establish current song as SongObj
-            //setSongObj: function() {
-            //    if (SongPlayer.currentSong === "") {
-            //       songObj = {title: "no title", artist: "no artist"};
-            //    } else {
-            //        songObj = {title: currentSong.title, artist: currentSong.artist};
-            //}
-            
-            //var currentSong = SongPlayer.currentSong;
-            //var songTest = "eureka!";
-    
-            //function that records a metric object by pushing it to the $rootScope array
-            registerSongPlay: function(songObj) {
-                //add time to event register
-                songObj['playedAt'] = moment(new Date());
-                $rootScope.songPlays.push(songObj);
-            },
-            
-            listSongsPlayed: function() {
-                var songs = ["test5"];
-                angular.forEach($rootScope.songPlays, function(song) {
-                    songs.push(song.title);
-                });
-                return songs;
-            }
-        };    
+        
+        //establish current song as SongObj
+        //setSongObj: function() {
+        //    if (SongPlayer.currentSong === "") {
+        //       songObj = {title: "no title", artist: "no artist"};
+        //    } else {
+        //        songObj = {title: currentSong.title, artist: currentSong.artist};
+        //}
+
+        //function that records a metric object by pushing it to the $rootScope array
+        Metric.registerSongPlay = function(songObj) {
+            //add time to event register
+            songObj['playedAt'] = moment(new Date());
+            $rootScope.songPlays.push(songObj);
+        };
+        
+        Metric.listSongsPlayed = function() {
+            var songs = ["test5"];
+            angular.forEach($rootScope.songPlays, function(song) {
+                songs.push(song.title);
+            });
+            return songs;
+        };
+        
+        
+        return Metric;    
     }
     
     angular
